905028aa91276c9b9673ec6b6fdb0947f91a067f
Nannan Yao <nansquared@gmail.com>
2016-07-08 08:57:38 -0400

    Completed fifth challenge

--- ExerciseQuestion.py
+++ ExerciseQuestion.py
@@ -119,6 +119,40 @@ def ConvertToTuple(sequenceNum):
     tupleOfNum = sequenceNum
     print tupleOfNum
 
+#----------------------------------#
+"""
+
+Question 5
+Level 1
+
+Question:
+Define a class which has at least two methods:
+getString: to get a string from console input
+printString: to print the string in upper case.
+Also please include simple test function to test the class methods.
+
+Hints:
+Use __init__ method to construct some parameters
+
+"""
+
+
+class WordManipulator():
+    def __init__(self):
+        self.word = ""
+
+    def getString(self):
+        self.word = raw_input()
+
+    def printString(self):
+        print self.word.upper()
+
+def TestWordManipulatorFunctions():
+    word = WordManipulator()
+    word.getString()
+    word.printString()
+
+
 def main():
     FindRangeOfNumbers(2000, 3200, 7, 5)
     print Factorial(5)
