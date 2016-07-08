ab4c050ac085ed4595b324fb9363b7881577bc87
Nannan Yao <nansquared@gmail.com>
2016-07-07 23:02:56 -0400

    Completed question 3 of exercises

--- ExerciseQuestion.py
+++ ExerciseQuestion.py
@@ -52,6 +52,34 @@ def Factorial(number):
         return 1
     return (number * Factorial(number - 1))
 
+#----------------------------------------#
+"""
+Question 3
+Level 1
+
+Question:
+With a given integral number n, write a program to generate a dictionary that
+contains (i, i*i) such that is an integral number between 1 and n
+(both included).
+and then the program should print the dictionary.
+
+Suppose the following input is supplied to the program:
+8
+Then, the output should be:
+{1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
+"""
+
+"""
+
+Maps the index in a dictionary to its squared value
+
+"""
+def MapToSquare(maxNum):
+    squaredIndices = dict()
+    for i in range(1, maxNum + 1):
+        squaredIndices[i] = i * i
+    print squaredIndices
+
 def main():
-    #FindRangeOfNumbers(2000, 3200, 7, 5)
+    FindRangeOfNumbers(2000, 3200, 7, 5)
     print Factorial(5)
